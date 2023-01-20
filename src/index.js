8721c749e4d7dda41de3c72b9cc2df2fc533f597
Nastasiia03 <abernatskao@gmail.com>
2023-01-20 19:58:20 +0200

    amended

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 import './css/styles.css';
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const DELAY = 300;
+
+const form = document.querySelector("#search-form");
+const input = document.querySelector(".search-input");
+const gallery = document.querySelector(".gallery")
+
+form.addEventListener("submit", onSearch);
+
+function onSearch(evt) {
+    evt.preventDefault();
+    const name = input.value;
+    if (name === '') {
+        return (gallery.innerHTML = '');
+    };
+        
+    searchImages(name)
+        .then(images => {
+            gallery.innerHTML = '';
+          // if (images.length >= 1) {
+            return gallery.insertAdjacentHTML("beforeend", getImages(images));
+          // };
+        })
+        .catch(noImages);
+}
+
+async function searchImages(name) {
+    const key = "32968431-e7a09705e2056856a618066e0";
+    const response = await axios.get(`https://pixabay.com/api/?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`);
+    return response.data.hits;
+}
+
+
+
+function getImages(images) {
+    return images.map(({ webformatURL, tags, likes, views, comments, downloads }) =>
+        `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+  </div>
+</div>`).join("");
+}
+
+function noImages() {
+  gallery.innerHTML = '';
+ Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+}
